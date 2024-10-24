5185642a004e4aaf77acde82ceb2c8df1f9e5dd5
thirumurug0xan <thirumurugann28@gmail.com>
2024-10-24 18:23:51 +0000

    adding some comment

--- Hashing.py
+++ Hashing.py
@@ -2,7 +2,7 @@ import hashlib
 
 class Hashing:
 	def __init__(self,user_input_location:str,user_hash:str,mode:str):
-		self.location_of_file = user_input_location
+		self.location_of_file = user_input_location #inducates where the file located
 		self.hash_mode = mode
 		self.hash_value = user_hash
 	def sha256(self,loc):
