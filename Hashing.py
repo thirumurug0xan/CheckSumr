4a7e127168ab249fd8fbd3a8a8ea5fa28faf2d0e
thirumurug0xan <thirumurugann28@gmail.com>
2024-10-24 18:18:30 +0000

    adding on extra param on init

--- Hashing.py
+++ Hashing.py
@@ -1,11 +1,12 @@
 import hashlib
 
 class Hashing:
-	def __init__(self,user_input_location:str,mode:str):
+	def __init__(self,user_input_location:str,user_hash:str,mode:str):
 		self.location_of_file = user_input_location
 		self.hash_mode = mode
+		self.hash_value = user_hash
 	def sha256(self,loc):
-		#some hashlib function
+		return #some boolean
 	def sha512(self,loc):
 		#some hashlib fuction
 	def md5(self,loc):
