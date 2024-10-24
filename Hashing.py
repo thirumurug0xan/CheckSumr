2eab431e874550c968dfc935dec207dc70b8a3c3
thirumurug0xan <thirumurugann28@gmail.com>
2024-10-24 17:36:10 +0000

    abstract sha256,sha512

--- Hashing.py
+++ Hashing.py
@@ -4,3 +4,7 @@ class Hashing:
 	def __init__(self,user_input_location:str,mode:str):
 		self.location_of_file = user_input_location
 		self.hash_mode = mode
+	def sha256(self,loc):
+		#some hashlib function
+	def sha512(self,loc):
+		#some hashlib fuction
